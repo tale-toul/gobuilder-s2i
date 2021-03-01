bb88ad00c487b230f9a66c319bc199938bb8688a
Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
2021-03-01 18:51:48 +0100

    First commit
    Signed-off-by: Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>

---
+++ simple-web.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/",response)
+	log.Fatal(http.ListenAndServe(":8080",nil))
+}
+
+func response(writer http.ResponseWriter, request *http.Request) {
+	outMsg := []byte("This is the root of the web site")
+	_, err := writer.Write(outMsg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
