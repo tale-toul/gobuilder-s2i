6339f80f052302cde680a9ede4e03e6f34555acf
Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
2021-03-05 19:14:51 +0100

    Small modification to output message when hitting /
    Signed-off-by: Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>

--- simple-web.go
+++ simple-web.go
@@ -11,7 +11,7 @@ func main() {
 }
 
 func response(writer http.ResponseWriter, request *http.Request) {
-	outMsg := []byte("This is the root of the web site")
+	outMsg := []byte("This is the root of the web site.  Nothing else to see here, move on")
 	_, err := writer.Write(outMsg)
 	if err != nil {
 		log.Fatal(err)
